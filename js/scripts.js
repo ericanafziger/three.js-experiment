bee4880b013313515fe533767df9495fd6455fa2
Erica Nafziger <ericakait@Ericas-iMac.local>
2017-02-23 21:36:03 -0800

    add fade-in animate-left and animate-right animations -- animate-right not working properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,52 @@
-var renderer,
-    scene,
-    camera,
-    myCanvas = document.getElementById('three-canvas');
+$(document).ready(function() {
 
+  /* Every time the window is scrolled ... */
+  $(window).scroll( function(){
 
-window.onresize = function() {
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 0.1, 1000 );
-}
+    /* Check the location of each animate-in elements */
+    $('.animate-in').each( function(i){
 
-// Renderer
-renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize( window.innerWidth, window.innerHeight);
-myCanvas.appendChild( renderer.domElement );
-renderer.setClearColor(0x191919);
-renderer.setPixelRatio(window.devicePixelRatio);
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-// Scene
-scene = new THREE.Scene();
+      /* If the object is completely visible in the window, fade it it */
+      if( bottom_of_window > bottom_of_object ){
 
-// Camera
-camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 0.1, 1000 );
+          $(this).animate({'opacity':'1'},400);
 
-// Lights
-var light = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(light);
+      }
 
-var light2 = new THREE.PointLight(0xffffff, 0.5);
-scene.add(light2);
+    });
 
-// Geometry
-var sphere = new THREE.SphereGeometry( 80, 10, 10 );
-var torusKnotGeometry = new THREE.TorusKnotGeometry( 80, 15, 10, 60 );
+    /* Check the location of animate-left elements */
+    $('.animate-left').each( function(i){
 
-// Material
-var material = new THREE.MeshLambertMaterial({
-  color: 0xDEDEDE,
-  wireframe: true,
-  transparent: true,
-  opacity: 0.4
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, move it it */
+      if( bottom_of_window > bottom_of_object ){
+
+          $(this).animate({'margin-left':'0', 'opacity':'1'},600);
+
+      }
+
+    });
+    /* Check the location of animate-right elements */
+    $('.animate-right').each( function(i){
+
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, move it it */
+      if( bottom_of_window > bottom_of_object ){
+
+          $(this).animate({'margin-right':'0', 'opacity':'1'},600);
+
+      }
+
+    });
+
+  });
+
 });
-
-// Creating and adding shapes
-var torusKnot = new THREE.Mesh( torusKnotGeometry, material );
-var sphere = new THREE.Mesh( sphere, material );
-torusKnot.position.z = -500;
-sphere.position.z = -500;
-scene.add( torusKnot );
-// scene.add( sphere );
-
-
-function render() {
-  requestAnimationFrame( render );
-  torusKnot.rotation.x += 0.007;
-  torusKnot.rotation.y += 0.01;
-  sphere.rotation.x += 0.007;
-  sphere.rotation.y += 0.01;
-  renderer.render( scene, camera );
-}
-render();
